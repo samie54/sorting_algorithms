1823851de1a4c3a66dc02d012c96b183658fee17
samie54 <126026530+samie54@users.noreply.github.com>
2023-07-29 08:59:14 -0700

    Task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,31 +1,45 @@
 #include "sort.h"
 
 /**
- * selection_sort  sorts selection sort list.
- * @array: printed array
- * @size: Number of elements in array.
+ * swap_ints - Two integers in array swaps
+ * @a: first swapped integer
+ * @b: second swapped integer
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * selection_sort - array of integers swaps in ascending order
+ * utilizing selection sort algorithm
+ * @array: array of integers
+ * @size: size of array
+ *
+ * Description: arrays after each swap prints
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, c, less, tmp, swap;
+	int *min;
+	size_t i, j;
 
-	if (array == NULL)
+	if (array == NULL || size < 2)
 		return;
-	for (i = 0; i < size; i++)
+
+	for (i = 0; i < size - 1; i++)
 	{
-		for (less = i, c = i; c < size; c++)
-			if (array[c] < array[less])
-			{
-				less = c;
-				swap = 1;
-			}
-		if (swap == 1)
+		min = array + i;
+		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? (array + j) : min;
+
+		if ((array + i) != min)
 		{
-			tmp = array[less];
-			array[less] = array[i];
-			array[i] = tmp;
+			swap_ints(array + i, min);
 			print_array(array, size);
-			swap = 0;
 		}
 	}
 }
